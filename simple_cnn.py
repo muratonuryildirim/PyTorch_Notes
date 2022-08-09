70c6fd2c4625393e3aaaa0423efefec9fd182931
Murat Onur Yildirim <muratonuryildirim@gmail.com>
2022-08-09 18:02:27 +0200

    update simple_cnn.py

--- simple_cnn.py
+++ simple_cnn.py
@@ -6,6 +6,50 @@ from torch.utils.data import DataLoader
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 
+# set device
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# hyperparameters
+in_channels = 1
+num_classes = 10
+learning_rate = 0.001
+batch_size = 64
+num_epochs = 1
+load_model = False
+
+
+def load_checkpoint(checkpoint):
+    print('Checkpoint loaded.')
+    model.load_state_dict(checkpoint['state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer'])
+
+
+def save_checkpoint(state, filename='my_checkpoint.pth.tar'):
+    print('Checkpoint saved.')
+    torch.save(state, filename)
+
+
+def check_accuracy(loader, model):
+    if loader.dataset.train:
+        print('Checking accuracy on training data...')
+    else:
+        print('Checking accuracy on test data...')
+    num_correct = 0
+    num_samples = 0
+    model.eval()  # evaluation state on, train state off
+
+    with torch.no_grad():
+        for x, y in loader:
+            x = x.to(device=device)
+            y = y.to(device=device)
+
+            scores = model(x)
+            _, predictions = scores.max(1)
+            num_correct += (predictions == y).sum()
+            num_samples += predictions.size(0)
+        print(f'Got {num_correct} / {num_samples} with accuracy {float(num_correct)/float(num_samples)*100}')
+    model.train()  # evaluation state off, train state on
+
 
 # create CNN
 class CNN(nn.Module):
@@ -36,24 +80,13 @@ class CNN(nn.Module):
         x = self.pool(x)
         x = x.reshape(x.shape[0], -1)
         x = self.fc1(x)
-
         return x
 
 
 # sanity check
-model = CNN(1, 10)  # number of channels, num_classes
+model_X = CNN(1, 10)  # number of channels, num_classes
 x = torch.randn(64, 1, 28, 28)
-print(model(x).shape)
-
-# set device
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-# hyperparameters
-in_channels = 1
-num_classes = 10
-learning_rate = 0.001
-batch_size = 64
-num_epochs = 1
+print(model_X(x).shape)
 
 # load data
 train_dataset = datasets.MNIST(root='dataset/', train=True, transform=transforms.ToTensor(), download=True)
@@ -69,7 +102,14 @@ criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
 # train network
+print(f'Training started with {num_epochs} epoch(s)')
+
+if load_model:
+    load_checkpoint(torch.load('my_checkpoint.pth.tar'))
+
 for epoch in range(num_epochs):
+
+    losses = []
     for batch_idx, (data, targets) in enumerate(train_loader):
         data = data.to(device=device)
         targets = targets.to(device=device)
@@ -77,37 +117,21 @@ for epoch in range(num_epochs):
         # forward
         scores = model(data)
         loss = criterion(scores, targets)
+        losses.append(loss.item())
 
         # backward
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
+    mean_loss = sum(losses) / len(losses)
+    print(f'Loss at epoch {epoch} was {mean_loss:.5f}')
+
+    # checkpoint
+    if epoch % 2 == 0:
+        checkpoint = {'state_dict': model.state_dict(), 'optimizer': optimizer.state_dict()}
+        save_checkpoint(checkpoint)
 
 # accuracy check
-def check_accuracy(loader, model):
-    if loader.dataset.train:
-        print('Checking accuracy on training data...')
-    else:
-        print('Checking accuracy on test data...')
-    num_correct = 0
-    num_samples = 0
-    model.eval()  # evaluation state on, train state off
-
-    with torch.no_grad():
-        for x, y in loader:
-            x = x.to(device=device)
-            y = y.to(device=device)
-
-            scores = model(x)
-            _, predictions = scores.max(1)
-            num_correct += (predictions == y).sum()
-            num_samples += predictions.size(0)
-
-        print(f'Got {num_correct} / {num_samples} with accuracy {float(num_correct)/float(num_samples)*100}')
-
-    model.train()  # evaluation state off, train state on
-
-
 check_accuracy(train_loader, model)
 check_accuracy(test_loader, model)
