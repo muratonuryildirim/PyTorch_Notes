25b2b2a02e6359462fcb1fb9c15607b761767ede
Murat Onur Yildirim <muratonuryildirim@gmail.com>
2022-08-09 18:02:49 +0200

    update simple_fullynet.py

--- simple_fullynet.py
+++ simple_fullynet.py
@@ -6,6 +6,51 @@ from torch.utils.data import DataLoader
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 
+# set device
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# hyperparameters
+input_size = 784
+num_classes = 10
+learning_rate = 0.001
+batch_size = 64
+num_epochs = 5
+load_model = False
+
+
+def load_checkpoint(checkpoint):
+    print('Checkpoint loaded.')
+    model.load_state_dict(checkpoint['state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer'])
+
+
+def save_checkpoint(state, filename='my_checkpoint.pth.tar'):
+    print('Checkpoint saved.')
+    torch.save(state, filename)
+
+
+def check_accuracy(loader, model):
+    if loader.dataset.train:
+        print('Checking accuracy on training data...')
+    else:
+        print('Checking accuracy on test data...')
+    num_correct = 0
+    num_samples = 0
+    model.eval()  # evaluation state on, train state off
+
+    with torch.no_grad():
+        for x, y in loader:
+            x = x.to(device=device)
+            y = y.to(device=device)
+            x = x.reshape(x.shape[0], -1)
+
+            scores = model(x)
+            _, predictions = scores.max(1)
+            num_correct += (predictions == y).sum()
+            num_samples += predictions.size(0)
+        print(f'Got {num_correct} / {num_samples} with accuracy {float(num_correct)/float(num_samples)*100}')
+    model.train()  # evaluation state off, train state on
+
 
 # create fullynet
 class NN(nn.Module):
@@ -19,77 +64,58 @@ class NN(nn.Module):
         x = self.fc2(x)
         return x
 
-
 # sanity check
-model = NN(784, 10) #number of features, num_classes
-x = torch.randn(64,784) #num of observation, number of features
-print(model(x).shape)
+model_X = NN(784, 10)  # number of features, num_classes
+x = torch.randn(64, 784)  # num of observation, number of features
+print(f'shape model_X: {model_X(x).shape}')
 
-#set device
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-#hyperparameters
-input_size = 784
-num_classes = 10
-learning_rate = 0.001
-batch_size = 64
-num_epochs = 1
-
-#load data
+# load data
 train_dataset = datasets.MNIST(root='dataset/', train=True, transform=transforms.ToTensor(), download=True)
 test_dataset = datasets.MNIST(root='dataset/', train=False, transform=transforms.ToTensor(), download=True)
 train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=True)
 
-#initialize network
+# initialize network
 model = NN(input_size=input_size, num_classes=num_classes).to(device)
 
-#loss and optimizer
+# loss and optimizer
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
-#train network
+# train network
+print(f'Training started with {num_epochs} epoch(s)')
+
+if load_model:
+    load_checkpoint(torch.load('my_checkpoint.pth.tar'))
+
 for epoch in range(num_epochs):
+
+    losses = []
     for batch_idx, (data, targets) in enumerate(train_loader):
         data = data.to(device=device)
         targets = targets.to(device=device)
 
-        #get to correct shape (flatten an image (64,1,28,28) to (64,284))
+        # get to correct shape (flatten an image (64,1,28,28) to (64,284))
         data = data.reshape(data.shape[0], -1)
 
-        #forward
+        # forward
         scores = model(data)
         loss = criterion(scores, targets)
+        losses.append(loss.item())
 
-        #backward
+        # backward
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
-#accuracy check
-def check_accuracy(loader, model):
-    if loader.dataset.train:
-        print('Checking accuracy on training data...')
-    else:
-        print('Checking accuracy on test data...')
-    num_correct = 0
-    num_samples = 0
-    model.eval() #evaluation state on, train state off
+    mean_loss = sum(losses)/len(losses)
+    print(f'Loss at epoch {epoch} was {mean_loss:.5f}')
 
-    with torch.no_grad():
-        for x, y in loader:
-            x = x.to(device=device)
-            y = y.to(device=device)
-            x = x.reshape(x.shape[0], -1)
+    # checkpoint
+    if epoch % 2 == 0:
+        checkpoint = {'state_dict': model.state_dict(), 'optimizer': optimizer.state_dict()}
+        save_checkpoint(checkpoint)
 
-            scores=model(x)
-            _, predictions = scores.max(1)
-            num_correct += (predictions == y).sum()
-            num_samples += predictions.size(0)
-
-        print(f'Got {num_correct} / {num_samples} with accuracy {float(num_correct)/float(num_samples)*100}')
-
-    model.train() #evaluation state off, train state on
-
+# accuracy check
 check_accuracy(train_loader, model)
 check_accuracy(test_loader, model)
